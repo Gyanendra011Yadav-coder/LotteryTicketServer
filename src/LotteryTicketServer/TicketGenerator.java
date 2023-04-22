dbb9151a26f6894356ebe42ecdc5c0a6de3f3296
DESKTOP-FRL0M1V\Gyanendra-Yadav <gyanendra.yadav@deltatech.gg>
2023-04-22 14:27:48 +0530

    Creating new ticket adding getter , setter and constructors to create tickets.

--- src/LotteryTicketServer/TicketGenerator.java
+++ src/LotteryTicketServer/TicketGenerator.java
@@ -5,8 +5,40 @@ import java.io.Serializable;
 /**
  * Created By:  Gyanendra_Yadav
  * on 2023-04-22,Apr,2023
- * in Project: SerializedTicketGeneratorSystem
+ * in Project: SerializedTicketGeneratorGeneratorSystem
  */
 public class TicketGenerator implements Serializable {
+    private String sellerCode;
+    private int[] numbers;
 
+    public TicketGenerator(String sellerCode, int[] numbers) {
+        this.sellerCode = sellerCode;
+        this.numbers = numbers;
+    }
+
+    public String getSellerCode() {
+        return sellerCode;
+    }
+
+    public void setSellerCode(String sellerCode) {
+        this.sellerCode = sellerCode;
+    }
+
+    public int[] getNumbers() {
+        return numbers;
+    }
+
+    public void setNumbers(int[] numbers) {
+        this.numbers = numbers;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(sellerCode).append(": ");
+        for (int number : numbers) {
+            sb.append(number).append(" ");
+        }
+        return sb.toString();
+    }
 }
