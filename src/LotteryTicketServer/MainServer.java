b71c78dad5ccf0a83e965b2bd01b3caf573a9a80
DESKTOP-FRL0M1V\Gyanendra-Yadav <gyanendra.yadav@deltatech.gg>
2023-04-22 13:55:25 +0530

    Adding code to generate servers and to create create tickets as well.

--- src/LotteryTicketServer/MainServer.java
+++ src/LotteryTicketServer/MainServer.java
@@ -1,9 +1,49 @@
 package LotteryTicketServer;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created By:  Gyanendra_Yadav
  * on 2023-04-22,Apr,2023
- * in Project: SerializedTicketSystem
+ * in Project: SerializedTicketGeneratorSystem
  */
-public class MainServer {
+public class MainServer implements Serializable {
+    private List<TicketGenerator> TicketGenerators;
+    private MainServer secondaryServer;
+    private boolean isPrimary;
+
+    public MainServer() {
+        this.TicketGenerators = new ArrayList<>();
+    }
+
+    public void addTicketGenerator(TicketGenerator TicketGenerator) {
+        this.TicketGenerators.add(TicketGenerator);
+    }
+
+    public List<TicketGenerator> getTicketGenerators() {
+        return this.TicketGenerators;
+    }
+
+    public void setSecondaryServer(MainServer secondaryServer) {
+        this.secondaryServer = secondaryServer;
+    }
+
+    public void setIsPrimary(boolean isPrimary) {
+        this.isPrimary = isPrimary;
+    }
+
+    public void replicate() {
+        // Only the primary server replicates its data to the secondary
+        if (isPrimary) {
+            // Get the TicketGenerators from this server
+            List<TicketGenerator> TicketGeneratorsToReplicate = getTicketGenerators();
+
+            // Replicate the TicketGenerators to the other server
+            for (TicketGenerator TicketGenerator : TicketGeneratorsToReplicate) {
+                secondaryServer.addTicketGenerator(TicketGenerator);
+            }
+        }
+    }
 }
--- src/LotteryTicketServer/TicketGenerator.java
+++ src/LotteryTicketServer/TicketGenerator.java
@@ -1,9 +1,12 @@
 package LotteryTicketServer;
 
+import java.io.Serializable;
+
 /**
  * Created By:  Gyanendra_Yadav
  * on 2023-04-22,Apr,2023
- * in Project: SerializedTicketSystem
+ * in Project: SerializedTicketGeneratorSystem
  */
-public class TicketGenerator {
+public class TicketGenerator implements Serializable {
+
 }
